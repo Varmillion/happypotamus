433828c326195cf8985e4ef7793d68faad111471
harnoor singh <harnoor.singh319@gmail.com>
2016-12-17 09:43:26 +0530

    alpha version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 has_many :pins
@@ -8,4 +6,5 @@ has_many :comments, dependent: :destroy
 
 has_attached_file :avatar, :styles => { :medium => "200x200>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
